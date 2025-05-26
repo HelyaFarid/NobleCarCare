64842aebfe35365c8c59a2342729d9119d992cbc
Helya Farid <helyaffarid@gmail.com>
2025-05-26 21:15:16 +1000

    contact from changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 // Email sending route
 app.post('/send-email', async (req, res) => {
-    const { email, message } = req.body;
+    const { email, name, message } = req.body;
 
     if (!email || !message) {
         return res.status(400).send({ error: 'Email and message are required.' });
@@ -22,18 +22,18 @@ app.post('/send-email', async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: 'smtp.ionos.com',
       port: 587,
-      secure:false, // Use your email service (e.g., Gmail, Outlook)
+      secure:false, 
         auth: {
           user: process.env.SMTP_USER ,
-          pass: process.env.SMTP_PASS // Replace with your email password or app password
+          pass: process.env.SMTP_PASS 
         }
     });
 
     const mailOptions = {
-        from: 'info@noblecarcare.net', // Replace with your email
-        to: 'info@noblecarcare.net', // Send to the provided email
+        from: 'info@noblecarcare.net', 
+        to: 'info@noblecarcare.net',
         subject: 'Message from Noble Car Care',
-        text: message
+        text: 'This message is from ' + name + '<br/>' + message
     };
 
     try {
@@ -54,49 +54,3 @@ app.listen(PORT, () => {
 
 
 
-
-
-
-//console.log('hi')
-//app.post('/', function(req, res) {
-   // console.log('hi');
-    //const output = `
-    //  <p>You have a new contact request</p>
-     // <h3>contact details</h3>
-     // <ul>
-      //  <li>Email: ${req.body.email}</li>
-      //</ul>
-     // <h3>Message</h3>
-     // <p>${req.body.message}</p>
-   // `;
-  
-
-    /*let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      host: 'mail.domain.com',
-      port: 465,
-      tls: {
-        rejectUnauthorized: false, //NOTE: you only need to set rejectUnauthorized to false if you are running on a local server, you can remove it after testing
-      }
-    });
-  
-    let mailOptions = {
-      from: `nodemailer contact ${req.body.email}`,
-      to: 'info@domain.com',
-      subject: 'User Form Contact',
-      html: output
-    };
-  
-    transporter.sendMail(mailOptions, function(error, info) {
-      if (error) {
-        return console.log(error);
-      }
-  
-      console.log('Message sent: %s', info.messageId);
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  
-      res.send({
-        msg: 'Email has been sent!'
-      });
-    });*/
-  //});
